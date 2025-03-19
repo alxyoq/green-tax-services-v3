a066f67652e35e6dffd4e38c9031575c35d5262f
alxyoq <alxyoq@gmail.com>
2025-03-19 12:48:21 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['images.unsplash.com', 'web-assets.same.dev'],
     unoptimized: true, // Required for static export
   },
   output: 'export', // Static HTML export
